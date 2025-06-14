eef154d4daf18af0e9fa947b061424f3f718804d
Fernando Martins <fernandom.adm@gmail.com>
2025-06-13 22:03:28 -0300

    Iniciando aprendizado em API

---
+++ Requisição API/RichAndMorth.js
@@ -0,0 +1,38 @@
+function listarTodos() {
+    var xhttp = new XMLHttpRequest();
+
+    // definindo o tipo de retorno do meu objeto xhttp
+    xhttp.responseType = "json";
+
+    xhttp.onreadystatechange = function () {
+
+        // status da requisição já retornada para o browser
+        if (this.readyState == 4) {
+            console.log(xhttp);
+
+            const listaPersonagens = xhttp.response.results;
+            const divResultado = document.getElementById('resultado');
+
+            listaPersonagens.forEach(personagem => {
+                divResultado.innerHTML +=  `
+                    <div class="card">
+                        <img src="${personagem.image}" alt="${personagem.name}">
+                        <h3>${personagem.name}</h3>
+                        <p>Espécie: ${personagem.species}</p>
+                        <p>Status: ${personagem.status}</p>
+                    </div>`
+            });
+
+            // if(this.status == 200){
+            //     alert("Registros encontrados com sucesso!");
+
+            // }else{
+            //     alert("Algo deu errado.");
+            // }
+        }  
+        
+    };
+
+    xhttp.open("GET", "https://rickandmortyapi.com/api/character");
+    xhttp.send();
+}
