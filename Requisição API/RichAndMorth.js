9401514df38c98c3ba7dbc5786e40fc2cd444fa7
Fernando Martins <fernandom.adm@gmail.com>
2025-06-22 21:35:30 -0300

    Finalizando exercicio API Rich and Morth

--- Requisição API/RichAndMorth.js
+++ Requisição API/RichAndMorth.js
@@ -35,6 +35,11 @@ function listarTodos() {
 
 function listarPorID(){
     var xhttp = new XMLHttpRequest();
+    const id = document.getElementById('idPersonagem').value; // ✔ Pega o valor do input
+    if(!id){ // ✔ Verifica a variável correta
+        alert("Id inválido. Tente novamente.");
+        return
+    }
 
     // definindo o tipo de retorno do meu objeto xhttp
     xhttp.responseType = "json";
@@ -43,13 +48,11 @@ function listarPorID(){
 
         // status da requisição já retornada para o browser
         if (this.readyState == 4) {
-            console.log(xhttp);
-
-            const personagens = xhttp.response.results;
-            const id = document.getElementById('number');
-
-            personagens.forEach(personagem => {
-                id.innerHTML +=  `
+            const divResultado = document.getElementById('resultado');
+            divResultado.innerHTML = ''; // ✔ Limpa o resultado anterior
+            if(this.status === 200){
+                const personagem = xhttp.response;// ✅ 3. Agora usa o objeto diretamente, pois "/character/:id" retorna um único objeto, e NÃO um array "results" :contentReference[oaicite:1]{index=1}
+                divResultado.innerHTML +=  `
                     <div class="card">
                         <img src="${personagem.image}" alt="${personagem.name}">
                         <h3>${personagem.name}</h3>
@@ -57,11 +60,12 @@ function listarPorID(){
                         <p>Espécie: ${personagem.species}</p>
                         <p>Status: ${personagem.status}</p>
                     </div>`
-            });
+            }else{
+                divResultado.innerHTML = `<p>Personagem não encontrado</p>`
+            }
         }  
-        
     };
 
-    xhttp.open("GET", "https://rickandmortyapi.com/api/character/"+id);
+    xhttp.open("GET", `https://rickandmortyapi.com/api/character/${id}`);
     xhttp.send();
 }
